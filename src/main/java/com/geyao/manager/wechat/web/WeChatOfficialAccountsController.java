68f1ba7bb5d2b74d3a7f1fcb364d34bcdaaddb1d
geyao <geyao@hebaochina.com>
2020-12-15 20:14:26 +0800

    12-15

---
+++ src/main/java/com/geyao/manager/wechat/web/WeChatOfficialAccountsController.java
@@ -0,0 +1,7 @@
+package com.geyao.manager.wechat.web;
+
+/**
+ * 微信公众号接口
+ */
+public interface WeChatOfficialAccountsController {
+}
---
+++ src/main/java/com/geyao/manager/wechat/web/impl/WeChatOfficialAccountsControllerImpl.java
@@ -0,0 +1,7 @@
+package com.geyao.manager.wechat.web.impl;
+
+import com.geyao.manager.wechat.web.WeChatOfficialAccountsController;
+
+public class WeChatOfficialAccountsControllerImpl implements WeChatOfficialAccountsController {
+
+}
